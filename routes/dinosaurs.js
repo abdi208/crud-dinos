2fc996088f9a8f5e43e1a9ac592f3a1fedcd23c6
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-10-08 14:27:36 -0700

    Add post route to form

--- routes/dinosaurs.js
+++ routes/dinosaurs.js
@@ -7,14 +7,26 @@ router.get('/', function(req, res) {
     var dinoData = JSON.parse(dinos)
     res.render('dinosaurs/index',{dinos: dinoData});
 });
+
+router.post('/', function(req, res) {
+    var dinos = fs.readFileSync('./dinos.json');
+    var dinoData = JSON.parse(dinos);
+    dinoData.push(req.body);
+    fs.writeFileSync('./dinos.json', JSON.stringify(dinoData));
+    res.redirect('/dinosaurs');
+})
+
 router.get('/new', function(req, res) {
     res.render('dinosaurs/new');
 })
+
 router.get('/:id', function(req, res) {
     var index = parseInt(req.params.id);
     var dinos = fs.readFileSync('./dinos.json');
     var dinoData = JSON.parse(dinos);
     res.render('dinosaurs/show',{ dino: dinoData[index]})
 });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(express.static('static'));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
     res.render('Home');
 })
 
-app.use('/dinosaurs', require('./routes/dinosaurs'))
+app.use('/dinosaurs', require('./routes/dinosaurs'));
 
 app.listen(3000, function(){
     console.log('server is running')
-})
+});
