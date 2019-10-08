1e7ac1c0b695c5f86263bff5c05b601e3ed1d841
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-10-08 14:03:45 -0700

    added show route and new route

--- routes/dinosaurs.js
+++ routes/dinosaurs.js
@@ -7,5 +7,14 @@ router.get('/', function(req, res) {
     var dinoData = JSON.parse(dinos)
     res.render('dinosaurs/index',{dinos: dinoData});
 });
+router.get('/new', function(req, res) {
+    res.render('dinosaurs/new');
+})
+router.get('/:id', function(req, res) {
+    var index = parseInt(req.params.id);
+    var dinos = fs.readFileSync('./dinos.json');
+    var dinoData = JSON.parse(dinos);
+    res.render('dinosaurs/show',{ dino: dinoData[index]})
+});
 
 module.exports = router;
