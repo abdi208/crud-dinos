5cde978d830dcf43beed905d013f7923474726f0
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-10-08 20:48:12 -0700

    created delete route

--- routes/cryptids.js
+++ routes/cryptids.js
@@ -17,6 +17,7 @@ router.post('/', function(req, res) {
     res.redirect("/cryptids");
 })
 
+//---show the form to edit----///
 router.get('/edit/:id',function(req, res) {
     var index = parseInt(req.params.id);
     var cryptids = fs.readFileSync('./cryptids.json');
@@ -24,17 +25,41 @@ router.get('/edit/:id',function(req, res) {
     res.render('cryptids/edit', {
         cryps: cryptidsData[index],
         crypsIndex: index})
-
+        console.log('you')
 })
+
+router.put('/:id', function(req, res) {
+    var index = parseInt(req.params.id);
+    var cryptids = fs.readFileSync('./cryptids.json');
+    var cryptidsData = JSON.parse(cryptids);
+    
+    cryptidsData[index] = req.body;
+    console.log('hello');
+    fs.writeFileSync('./cryptids.json', JSON.stringify(cryptidsData))
+    res.redirect(`/cryptids/${index}`)
+})
+
+router.delete("/:id", function(req, res) {
+    console.log('deleting dino at' + req.params.id);
+    var index = parseInt(req.params.id);
+    var cryptids = fs.readFileSync('./cryptids.json');
+    var cryptidsData = JSON.parse(cryptids);
+    var removeCryptid = cryptidsData.splice(index, 1);
+    console.log(`farwell ${removeCryptid.name}. you were great`)
+
+    fs.writeFileSync("./cryptids.json", JSON.stringify(cryptidsData))
+    res.redirect('/cryptids')
+})
+
 router.get('/new', function(req, res) {
     res.render('cryptids/new')
 })
 
-router.get("/:id", function(req, res) {
-    var index = parseInt(req.params.id)
-    var cryptids = fs.readFileSync('./cryptids.json');
-    var cryptidsData = JSON.parse(cryptids);
-    res.render('cryptids/show', {cryptid: cryptidsData[index]})
-})
+// router.get("/:id", function(req, res) {
+//     var index = parseInt(req.params.id)
+//     var cryptids = fs.readFileSync('./cryptids.json');
+//     var cryptidsData = JSON.parse(cryptids);
+//     res.render('cryptids/show', {cryptid: cryptidsData[index]})
+// })
 
 module.exports = router;
